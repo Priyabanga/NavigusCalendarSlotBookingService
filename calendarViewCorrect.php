d09ad75401f7b8c378df9ac481a3bd96dc2eb2c4
Priyabanga <priya.banga_ccv17@gla.ac.in>
2020-07-28 19:25:35 -0700

    calendar view

---
+++ calendarViewCorrect.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+include_once('php_include/connection.php');
+//include_once('date_time.php');
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <link href="css/tableview.css" rel="stylesheet" media="all" />
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script>
