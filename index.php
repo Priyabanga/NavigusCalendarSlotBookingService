88f571e45595bd6def0ac886c677aa554087b97d
Priyabanga <priya.banga_ccv17@gla.ac.in>
2020-07-28 17:34:29 -0700

    created header and link for registration page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/frontpage.css">
+<link rel="icon" href="images/kgtdevlogoresized.jpg" type="image/x-icon">
+<script>
+
+</script>
+</head>
+<body>
+<?php include_once("page_header.php"); ?>
+<div id="main">
+	<h3>Welcome to the Calendar Based Slot Booking system. Please click <a href="login_page.php">here</a> to login and make an appointment.</h3>
+
+	<h3>Don't have an account? Dont Worry, just Click <a href="registration_page.php">here</a> to register and enjoy the convenience of our caledar based slot booking system.</h3>
+</div>
+</body>
+</html>
