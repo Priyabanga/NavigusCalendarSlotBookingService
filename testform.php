8b5f6583c70495cc242f8bd8ac5cb97569b52ef2
Priyabanga <priya.banga_ccv17@gla.ac.in>
2020-07-28 18:39:41 -0700

    reference

---
+++ testform.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/registration.css">
+<script src="js/main.js"></script>
+<script src="js/ajax.js"></script>
+<script>
